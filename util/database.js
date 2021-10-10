13fcf8e6445ad62dc5dea1602772e8930bb2a93b
David-Hernan <71787287+David-Hernan@users.noreply.github.com>
2021-10-10 03:53:30 -0500

    database importada

---
+++ util/database.js
@@ -0,0 +1,10 @@
+const mysql = require('mysql2');
+
+const pool = mysql.createPool({
+    host: 'localhost',
+    user: 'root',
+    database: 'proy_bayer',
+    password: ''
+});
+
+module.exports = pool.promise();

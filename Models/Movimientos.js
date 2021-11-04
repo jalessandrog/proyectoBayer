8b6b631361ad35cd6b40c5c77f7af84cb7c1f4f6
David-Hernan <71787287+David-Hernan@users.noreply.github.com>
2021-11-04 15:07:47 -0600

    Buscar movimientos terminado

--- Models/Movimientos.js
+++ Models/Movimientos.js
@@ -6,7 +6,7 @@ module.exports = class Muestra {
         return db.execute("SELECT M.idMuestra, CONCAT(U.Nombres,' ', U.Apellidos) 'NombreUsuario', M.FechaDeUso, Mu.NombreMuestra, Mu.SP, M.Descarga, M.Sobrante FROM manipulan M, usuarios U, muestras Mu WHERE M.idMuestra = Mu.idMuestra AND M.idEmpleado = U.idEmpleado ORDER BY M.FechaDeUso DESC");
     }
     static fetchAllbyTerm(query) {
-        return db.execute("SELECT M.idMuestra, CONCAT(U.Nombres,' ', U.Apellidos) 'NombreUsuario', M.FechaDeUso, Mu.NombreMuestra, Mu.SP, M.Descarga, M.Sobrante FROM manipulan M, usuarios U, muestras Mu WHERE M.idMuestra = Mu.idMuestra AND M.idEmpleado = U.idEmpleado AND U.Nombres LIKE ? ORDER BY M.FechaDeUso DESC",['%'+query+'%'])
+        return db.execute("SELECT M.idMuestra, CONCAT(U.Nombres,' ', U.Apellidos) 'NombreUsuario', M.FechaDeUso, Mu.NombreMuestra, Mu.SP, M.Descarga, M.Sobrante FROM manipulan M, usuarios U, muestras Mu WHERE M.idMuestra = Mu.idMuestra AND M.idEmpleado = U.idEmpleado AND (U.Nombres LIKE ? OR U.Apellidos LIKE ? OR MU.NombreMuestra LIKE ?) ORDER BY M.FechaDeUso DESC",['%'+query+'%','%'+query+'%','%'+query+'%'])
     }
     
 
